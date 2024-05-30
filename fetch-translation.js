4c2780e0b1920e63df85d3eeb80de998f8fd3896
jadamconnor <j.adam.connor@gmail.com>
2024-05-30 00:33:15 -0500

    use AWS S3 to host content

--- fetch-translation.js
+++ fetch-translation.js
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import https from 'https';
 import { config } from 'dotenv';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import fetch from 'node-fetch';
+import { json } from 'react-router-dom';
 
 config();
 
@@ -10,31 +14,53 @@ const masterKey = process.env.CONTENT_SERVICE_MASTER_KEY;
 const accessKey = process.env.CONTENT_SERVICE_ACCESS_KEY;
 const filePath = './src/locales/en.json';
 
-https
-  .get(
-    `${url}/${binID}`,
-    {
-      headers: {
-        'X-Master-Key': masterKey,
-        'X-Access-Key': accessKey,
-      },
-    },
-    (response) => {
-      let data = '';
+const s3Client = new S3Client({
+  region: 'us-east-2', // Replace with your AWS region
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
 
-      // A chunk of data has been received.
-      response.on('data', (chunk) => {
-        console.log(chunk);
-        data += chunk;
-      });
+async function fetchJsonFromS3(bucket, key) {
+  try {
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    });
 
-      // The whole response has been received.
-      response.on('end', () => {
-        fs.writeFileSync(filePath, data);
-        console.log('Translation file downloaded and saved.');
-      });
-    },
-  )
-  .on('error', (err) => {
-    console.error('Error fetching the file:', err);
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+    }
+
+    const jsonContent = await response.json();
+    // console.log('JSON content:', jsonContent);
+    return jsonContent;
+  } catch (error) {
+    console.error('Error fetching JSON from S3:', error);
+    throw error;
+  }
+}
+
+const bucketName = 'aquachemcontent';
+const objectKey = 'en.json';
+
+fetchJsonFromS3(bucketName, objectKey)
+  .then((jsonContent) => {
+    console.log(jsonContent);
+    fs.writeFile(filePath, JSON.stringify(jsonContent, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log('File written successfully');
+      }
+    });
+    console.log('Translation file downloaded and saved.');
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.log('Error fetching JSON from S3:', error);
   });
